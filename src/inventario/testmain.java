fe3da7adf266caf691ca8f2f4a8a4215f1822ea4
danielx <danielx-25@hotmail.com>
2016-04-21 14:26:45 -0400

    modelo de inventario 2

--- src/inventario/testmain.java
+++ src/inventario/testmain.java
@@ -6,7 +6,10 @@ public class testmain {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
 		ModeloInventario1 modelo1 = new ModeloInventario1(1500, 70, 15);
-
+		System.out.println();
+		System.out.println();
+		ModeloInventario2 modelo2 = new ModeloInventario2(0, 1500, 70, 15);
+		
 	}
 
 }
