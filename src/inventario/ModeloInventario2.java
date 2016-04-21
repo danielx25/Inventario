7fabb6b334dcb371ad5562204daec409c3384c42
danielx <danielx-25@hotmail.com>
2016-04-20 22:18:42 -0300

    beta inventario 2

--- src/inventario/ModeloInventario2.java
+++ src/inventario/ModeloInventario2.java
@@ -1,5 +1,18 @@
 package inventario;
 
 public class ModeloInventario2 {
+	
+	private double demanda;
+	private double cOrdenar;// los costos
+	private double cInventario;
+	
+	private double cantidadOp; //cantidad optima en el inventario
+	private double cTotal;
+	
+	private double frecuencia;
+	private double tasa;
+	
+	private double x_L;//Cantidad máxima de unidades en agotamiento
+	private double x_S;//Inventario en mano, es decir lo que realmente se tiene.
 
 }
